88017780e0a5294faf272ab6df453277ee865844
shanlakshitha94 <shanlakshitha94@hotmail.com>
2022-12-19 19:31:19 +0530

    Steup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -79,10 +80,10 @@ app.get('/weather', (req, res) => {
                 address : req.query.address
             })
         })
-        
+
     })
 
-    
+
 })
 
 app.get('/products', (req, res) => {
@@ -106,6 +107,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log(`Example app listening on port 3000`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ` + port)
   })
